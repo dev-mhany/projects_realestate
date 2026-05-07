c6f6b12d7f216b19fc88ac1125971c37db5dd424
Muhammad Hany <dev.mhany@gmail.com>
2026-05-07 03:14:45 +0300

    Backup local changes [auto cleanup 2026-05-07]

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -2,13 +2,19 @@ import axios from "axios";
 
 export const baseUrl = 'https://bayut.p.rapidapi.com';
 
-export const fetchApi = async (url) => {
-  const { data } = await axios.get((url), {
+export const fetchApi = async (url, params) => {
+  const { data } = await axios.get(`${baseUrl}${url}`, {
+    params: {
+      ...params, // This will spread the query parameters you pass when you call fetchApi
+      hitsPerPage: '25',
+      page: '0',
+      lang: 'en'
+    },
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'x-rapidapi-key': 'ae4fe94479msh064591a75003e1ap17ad02jsn3af563dfabd2',
     },
   });
-    
+
   return data;
 }
